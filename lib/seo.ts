8517e666f4155ff1dcc2afba65c34a17ceb6d8cf
hymanzhu <hymanchu@outlook.com>
2024-08-10 17:39:12 +0800

    更新文字说明

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,7 +1,7 @@
 export const seo = {
-  title: 'Cali Castle | 开发者、设计师、细节控、创始人',
+  title: 'Hyman Zhu | 开发者、架构师、AI新人、梦想家',
   description:
-    '我叫 Cali，一名开发者，设计师，细节控，同时也是佐玩创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。',
+    '我是Hyman，一位充满激情的资深软件工程师兼架构师，在区块链技术和人工智能领域不断探索的新锐力量。',
   url: new URL(
     process.env.NODE_ENV === 'production'
       ? 'https://cali.so'
